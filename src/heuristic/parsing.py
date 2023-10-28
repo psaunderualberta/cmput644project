4845c7b24e70f1fec93ac5e1589b8078be90b25c
psaunderualberta <psaunder@ualberta.ca>
2023-10-28 16:16:59 -0600

    Updated parsing algorithm to Earley

--- src/heuristic/parsing.py
+++ src/heuristic/parsing.py
@@ -4,10 +4,5 @@ from src.heuristic.transformer import HeuristicTransformer
 
 
 def parse_heuristic(heuristic):
-    parser = Lark(
-        HEURISTIC_GRAMMAR,
-        start="heuristic",
-        parser="lalr",
-        transformer=HeuristicTransformer(),
-    )
-    return parser.parse(heuristic)
+    parser = Lark(HEURISTIC_GRAMMAR, start="heuristic")
+    return HeuristicTransformer().transform(parser.parse(heuristic))
